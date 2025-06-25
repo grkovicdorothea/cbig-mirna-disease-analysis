c455bd21255acd14a4d98e4673c7058d323abd86
Dorothea Grkovic <135038649+grkovicdorothea@users.noreply.github.com>
2025-06-25 14:34:44 +0300

    Update git_cbig_mirna_disease_analysis.py

--- git_cbig_mirna_disease_analysis.py
+++ git_cbig_mirna_disease_analysis.py
@@ -66,7 +66,6 @@ Use the tabs above to navigate between clustering results, similarity networks, 
 
     st.image(
         "https://drive.google.com/uc?export=view&id=1YhUT2Bv_uuJ5wgT00zq8Y5ioSf6XqiMu", 
-        caption="Overview of Disease Similarity Network", 
         use_column_width=True
     )
 
